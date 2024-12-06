2f3da623e790e5258da5d8f38edee4472bbcaed4
Максим <160880502+elow1111@users.noreply.github.com>
2024-12-06 17:08:13 +0300

    solution.py

--- 5/src/solution.py
+++ 5/src/solution.py
@@ -15,5 +15,19 @@ Base.metadata.create_all(engine)
 
 
 # BEGIN (write your solution here)
-
+def get_movies_with_directors(session):
+    stmt = (
+        select(Movie, Director)
+        .join(Director, Director.id == Movie.director_id)  # Уточняем, как соединяем таблицы
+        .order_by(Movie.title)
+    )
+    
+    results = session.execute(stmt).all()
+    
+    movies_with_directors = [
+        f"{movie.title} by {director.name}, released on {movie.release_date}, duration: {movie.duration} min, genre: {movie.genre}, rating: {movie.rating}"
+        for movie, director in results
+    ]
+    
+    return movies_with_directors
 # END
